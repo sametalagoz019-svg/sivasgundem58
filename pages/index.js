6d7dc05466ee8b8ee1adc170bd43d493b6237c2a
sametalagoz019-svg <sametalagoz019@gmail.com>
2026-03-25 20:36:44 +0300

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{padding:20,fontFamily:"Arial"}}>
+      <h1>Sivas Gündem 58</h1>
+      <p>Haberler yakında burada olacak...</p>
+    </div>
+  )
+}
