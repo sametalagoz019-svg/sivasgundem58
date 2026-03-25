380729e109f28d20fcbad618f3bb0c84bd26f838
sametalagoz019-svg <sametalagoz019@gmail.com>
2026-03-25 21:52:29 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,22 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+import { useState } from "react";
 
-module.exports = nextConfig
+export default function Home() {
+  const [news, setNews] = useState([]);
+
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Sivas Gündem 58</h1>
+
+      {news.length === 0 ? (
+        <p>Henüz haber yok</p>
+      ) : (
+        news.map((item, i) => (
+          <div key={i}>
+            <h3>{item.title}</h3>
+            <p>{item.content}</p>
+          </div>
+        ))
+      )}
+    </div>
+  );
+}
